bb53008534d91258033e330ef77a227ba670d01a
Vanessa Campoy <88167544+vanessacampoy@users.noreply.github.com>
2021-09-27 14:09:29 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var dataDoEvento = window.prompt('Informe data do evento: dd/mm/aa')
+var dataAtual = window.prompt ('Informe data atual: dd/mm/aa')
+
+if(dataAtual =<queDataAtual){
+    alert('Continue o cadastro')
+}
+
+else(data atual > que data do evento){
+    alert('O evento já aconteceu')
+}
+
+var cadastroDeParticipantes = window.prompt('Digite seu nome')
+var dataDeNascimento = window.prompt ('Digite sua data de nascimento')
+
+if(dataDeNascimento =>18){
+    alert('Continue seu cadastro')
+}
+else(dataDeNascimento <18){
+    alert('Idade menor do que a permitida no evento')
+}
+
+var numeroDeParticipantes = 100
+if(numero de participantes =<100){
+    alert('Cadastro realizado com sucesso!')
+}
+
+else(numero de participantes >que 100{
+    alert('Seu cadastro não pode ser concluído, número de participantes excedido.')
+}
